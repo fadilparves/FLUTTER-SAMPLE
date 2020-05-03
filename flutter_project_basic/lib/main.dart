d542863d50c9b864976cc83c11cdab7dd22ed2a8
Mohamad Fadil <xymerz1@gmail.com>
2020-05-03 15:31:47 +0800

    step 2

--- flutter_project_basic/lib/main.dart
+++ flutter_project_basic/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'),);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
