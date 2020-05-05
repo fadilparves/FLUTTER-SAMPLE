00f596fe10469b0410996690353f7dd89ab961f0
Mohamad Fadil <fadil@Mohamads-MacBook-Pro.local>
2020-05-05 21:25:07 +0800

    add bottom slider

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,10 +1,12 @@
-import './widgets/user_transactions_state.dart';
 import 'package:flutter/material.dart';
+
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,22 +16,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+      Transaction(
+        id: 't1',
+        title: 'New shoes',
+        amount: 69.66,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 15.54,
+        date: DateTime.now(),
+      ),
+    ];
+
+    void _addNewTransaction(String title, double amount) {
+      final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString(),
+      );
+
+      setState(() {
+        _userTransactions.add(newTx);
+      });
+    }
+
+    void _startNewAddTransaction(BuildContext ctx) {
+      showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransactions(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        },
+      );
+    }
   @override
   Widget build(BuildContext context) {
-    // String titleInput;
-    // String amountInput;
-
-    final titleController = TextEditingController();
-    final amountController = TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startNewAddTransaction(context),
           )
         ],
       ),
@@ -45,14 +86,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactionsState(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startNewAddTransaction(context),
       ),
     );
   }
