58384111999a806534fa05bf9447ff6502a70a01
Mohamad Fadil <xymerz1@gmail.com>
2020-05-05 15:02:14 +0800

    step 3

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -48,7 +48,48 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                elevation: 5,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 50,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(tx.date.toString(),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
