dd928c74b539038f05a874e23a8e3c0bbb5c60a7
Mohamad Fadil <xymerz1@gmail.com>
2020-05-13 15:55:22 +0800

    can take image in app

--- great_places_app/lib/screens/places_list_screen.dart
+++ great_places_app/lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './add_place_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -17,7 +19,25 @@ class PlacesListScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Consumer<GreatPlaces>(
+          child: Text('Got no places yet, start adding some!'),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : ListView.builder(
+                  itemCount: greatPlaces.items.length,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(
+                        greatPlaces.items[i].image,
+                      ),
+                    ),
+                    title: Text(greatPlaces.items[i].title),
+                    onTap: () {
+                      //....
+                    },
+                  ),
+                ),
+        ),
       ),
     );
   }
--- great_places_app/lib/widgets/image_input.dart
+++ great_places_app/lib/widgets/image_input.dart
@@ -5,6 +5,9 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+  
+  ImageInput(this.onSelectImage);
   @override
   _ImageInputState createState() => _ImageInputState();
 }
@@ -17,12 +20,16 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxHeight: 600,
     );
+    if (imageFile == null) {
+      return;
+    }
     setState(() {
       _storedImage = imageFile;
     });
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(imageFile.path);
     final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
   }
 
   @override
