c8df9cdd3aee30e082d15ff31a7f787d6cf33766
Mohamad Fadil <xymerz1@gmail.com>
2020-05-13 16:27:05 +0800

    added sqlite support

--- great_places_app/lib/providers/great_places.dart
+++ great_places_app/lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
 import 'dart:io';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -20,6 +21,26 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
