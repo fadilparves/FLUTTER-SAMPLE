f7028ec40dbb58578d4e7ad2990453279937f0c1
Mohamad Fadil <xymerz1@gmail.com>
2020-05-16 13:52:34 +0800

    read battery natively from android done

--- myapp/android/app/src/main/java/com/example/myapp/MainActivity.java
+++ myapp/android/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,6 +1,52 @@
 package com.example.myapp;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), "course.flutter.dev/battery").setMethodCallHandler(
+            new MethodCallHandler() {
+                @Override
+                public void onMethodCall(MethodCall call, Result result) {
+                    if (call.method.equals("getBatteryMethod")) {
+                        int batteryLevel = getBatteryLevel();
+                        if(batteryLevel != -1){
+                            result.success(batteryLevel);
+                        } else {
+                            result.error("ERROR", "COULD NOT FETCH BATTERY LEVEL", null );
+                        }
+                    } else {
+                        result.notImplemented();
+                    }
+                } 
+            }
+        );
+    }
+
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            batteryLevel = 100;
+        }
+        return batteryLevel;
+    }
 }
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -43,6 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _getBatteryLevel();
   }
 
   @override
